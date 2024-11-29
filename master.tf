3ed2ca532780740ad0ef74fd73d8559c1657af6b
hexqueller <dima.nesed@gmail.com>
2024-11-29 14:10:35 +0300

    Фиксы

--- master.tf
+++ master.tf
@@ -3,11 +3,11 @@ resource "selectel_vpc_project_v2" "project_1" {
 }
 
 resource "selectel_iam_serviceuser_v1" "serviceuser_1" {
-  name     = "Admin"
+  name     = "tf"
   password = "1qaz!QAZ"
   role {
     role_name  = "member"
-    scope      = "testProject"
+    scope      = "project"
     project_id = selectel_vpc_project_v2.project_1.id
   }
 }
