13638f3660c7bcd1c3114dd85a2b13ff24fbca1d
hexqueller <dima.nesed@gmail.com>
2024-12-05 14:05:48 +0300

    new module

--- main.tf
+++ main.tf
@@ -4,8 +4,6 @@ resource "selectel_vpc_keypair_v2" "keypair_1" {
   user_id    = selectel_iam_serviceuser_v1.serviceuser_1.id
 }
 
-# openstack_images_image_v2
-
 resource "openstack_compute_flavor_v2" "main-flavor" {
   name  = "main-flavor"
   ram   = "512"
@@ -13,10 +11,19 @@ resource "openstack_compute_flavor_v2" "main-flavor" {
   disk  = "5"
 }
 
+module "image_datasource" {
+  source     = "./modules/image_datasource"
+
+  depends_on = [
+    selectel_vpc_project_v2.project_1,
+    selectel_iam_serviceuser_v1.serviceuser_1
+  ]
+}
+
 resource "openstack_compute_instance_v2" "webserver" {
   name  = "webserver"
   flavor_id = openstack_compute_flavor_v2.main-flavor.id
-  image_id = ""
+  image_id = module.image_datasource.image_id
   key_pair = selectel_vpc_keypair_v2.keypair_1.name
 
   network {
---
+++ modules/image_datasource/vars.tf
@@ -0,0 +1,3 @@
+variable "image_name" {
+  default = "Ubuntu 24.04 LTS 64-bit"
+}
